ddaa873b5686b8dbcbc84fd34b257205ff5feb8c
Simone D <monatru3@hotmail.com>
2018-01-31 03:33:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+ var emptyArray=[];
+ for (var m=0; m<musicians.length; m++) {
+     emptyArray.push(musicians[m]+ " plays " +instruments[m])
+  }
+return emptyArray
+}
+
+function johnLennonFacts(array)  {
+var i=0
+while (i<array.length) {
+  array[i] += "!!!"
+  i++
+}
+return array
+}
+
+function iLoveTheBeatles(number) {
+var emptyArray=[];
+  do{
+    emptyArray.push("I love the Beatles!")
+    number++
+  } while (number<15);
+  return emptyArray
+}
