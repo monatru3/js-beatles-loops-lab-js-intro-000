de2dd263708f138074fbfe7d26cd0f79a52e5a7e
Simone D <monatru3@hotmail.com>
2018-01-31 03:12:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function theBeatlesPlay(musicians, instruments) {
+ var emptyArray=[];
+ for (var m=0; m<musicians.length; m++) {
+     emptyArray.push(musicians[m]+ " plays " +instruments[m])
+  }
+return emptyArray
+}
+
+function johnLennonFacts(array)  {
+var i=0
+while (i<array.length) {
+  array[i]+="!!!"
+}
+return array
+}
+
+function iLoveTheBeatles(number) {
+
+}
